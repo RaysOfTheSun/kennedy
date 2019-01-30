86cc48b5706fe0e2e7ad4bb6f3e484082d4506e2
Carl <rdelsol35@gmail.com>
2019-01-30 23:37:15 +0800

    Revert DB conn strings

--- routers/get-apparel.js
+++ routers/get-apparel.js
@@ -2,9 +2,9 @@ const router = require('express').Router();
 const mongoose = require('mongoose');
 const model = require('../api/models/apparel/apparel-model');
 const controller = require('../api/controllers/apparel/apparel-controller');
-const connString = 'mongodb://127.0.0.1:27017/kenny';
+const connString = process.env.MONGODB || 'mongodb://127.0.0.1:27017/kenny';
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB || connString, {useNewUrlParser: true});
+mongoose.connect(connString, {useNewUrlParser: true});
 
 router.get('/', controller.GetAll);
 router.get('/featured', controller.GetFeatured);
--- routers/get-bags.js
+++ routers/get-bags.js
@@ -2,9 +2,9 @@ const router = require('express').Router();
 const mongoose = require('mongoose');
 const model = require('../api/models/bags/bags-model');
 const controller = require('../api/controllers/bags/bags-controller');
-const connString = 'mongodb://127.0.0.1:27017/kenny';
+const connString = process.env.MONGODB || 'mongodb://127.0.0.1:27017/kenny';
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB || connString, {useNewUrlParser: true});
+mongoose.connect(connString, {useNewUrlParser: true});
 
 router.get('/', controller.GetAll);
 router.get('/featured', controller.GetFeatured);
