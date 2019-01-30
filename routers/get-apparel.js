2578ee367a8f5ab46bcfa0dc3d2e3ee0e9c956e9
Carl <rdelsol35@gmail.com>
2019-01-30 22:42:53 +0800

    Update components, assets and styles

---
+++ routers/get-apparel.js
@@ -0,0 +1,13 @@
+const router = require('express').Router();
+const mongoose = require('mongoose');
+const model = require('../api/models/apparel/apparel-model');
+const controller = require('../api/controllers/apparel/apparel-controller');
+const connString = process.env.MONGODB || 'mongodb://127.0.0.1:27017/kenny';
+mongoose.Promise = global.Promise;
+mongoose.connect(connString, {useNewUrlParser: true});
+
+router.get('/', controller.GetAll);
+router.get('/featured', controller.GetFeatured);
+router.get('/new', controller.GetNew);
+
+module.exports = router;
